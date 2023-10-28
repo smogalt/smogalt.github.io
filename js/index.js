9e1795c132a87931c395fc57c025dae26bcef154
smogalt <107054973+smogalt@users.noreply.github.com>
2023-10-27 20:57:45 -0400

    Changed UX

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function reveal_right() {
+    var reveals = document.querySelectorAll(".fade.right");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 400;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+function reveal_left() {
+    var reveals = document.querySelectorAll(".fade.left");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 400;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal_right);
+window.addEventListener("scroll", reveal_left);
+
